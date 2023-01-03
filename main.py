2ca35f7b3f3eda64fc0fc665570960ab0494d11e
Dev <devwithsmile007@gmail.com>
2023-01-03 23:54:56 +0530

    dont ask frustated rn

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
-my_client = pymongo.MongoClient("mongodb://localhost:27017/")
+host='test_mongodb'
+my_client = pymongo.MongoClient("mongodb://mongodb:27017/")
 mydb = my_client["chatapp"]
 my_rooms = mydb["all_rooms"]
 my_users = mydb["all_users"]
